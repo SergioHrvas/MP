30182e84daaf5f14a77be107ef2f2eafe56b910a
Sergio <72129125+SergioHrvas@users.noreply.github.com>
2021-06-30 16:12:25 +0200

    Delete Wally/build/Debug/GNU-Linux/src directory

--- Wally/build/Debug/GNU-Linux/src/PoliReg.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU-Linux/src/PoliReg.o: src/PoliReg.cpp include/Punto2D.h \
- include/PoliReg.h
-
-include/Punto2D.h:
-
-include/PoliReg.h:
--- Wally/build/Debug/GNU-Linux/src/wallymodulo.o.d
+++
@@ -1,6 +0,0 @@
-build/Debug/GNU-Linux/src/wallymodulo.o: src/wallymodulo.cpp \
- include/Punto2D.h include/PoliReg.h
-
-include/Punto2D.h:
-
-include/PoliReg.h:
